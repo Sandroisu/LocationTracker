ec97504566c69e0703cf1ec93dfa729d77dad85f
Sandroisu <sandroisu1411@gmail.com>
2020-06-22 16:45:05 +0300

    fixes

--- app/src/main/java/com/example/locationtracker/LocationService.java
+++ app/src/main/java/com/example/locationtracker/LocationService.java
@@ -5,15 +5,12 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
-import android.app.job.JobParameters;
-import android.app.job.JobService;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.IBinder;
 import android.os.Looper;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -72,12 +69,10 @@ public class LocationService extends Service {
             @Override
             public void onLocationResult(LocationResult locationResult) {
                 Location location = locationResult.getLastLocation();
-
                 if(location != null){
                     Toast.makeText(LocationService.this, ""+location.getLatitude(), Toast.LENGTH_SHORT).show();
                 }
                 super.onLocationResult(locationResult);
-
             }
         }, Looper.myLooper());
     }
--- app/src/main/java/com/example/locationtracker/MainActivity.java
+++ app/src/main/java/com/example/locationtracker/MainActivity.java
@@ -15,26 +15,12 @@ import com.google.android.gms.location.LocationRequest;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LocationRequest locationRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent serviceIntent = new Intent(this, LocationService.class);
         startForegroundService(serviceIntent);
-
-
     }
 
-    private void updateLocation() {
-
-    }
-
-    public void scheduleJob(View v){
-
-    }
-
-    public void cancelJob (View v){
-
-    }
 }
